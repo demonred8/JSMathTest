aa3f45027fd55a66c0f5719588c09ff670356f87
DemonRed <demonred8@gmail.com>
2021-09-27 18:56:28 +0300

    task4

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,15 +1,24 @@
-const calc_S_P = () => {
+const calc_S = () => {
   const S = document.getElementById("S");
-  const P = document.getElementById("P");
-  let circleDiameter = input1.value;
+  let a = Number(input1.value);
+  let b = Number(input2.value);
+  let c = Number(input3.value);
 
-  S.textContent =
-    "Площадь круга: " + (Math.PI * (circleDiameter * circleDiameter)) / 4;
-  P.textContent = "Длина окружности: " + Math.PI * circleDiameter;
+  let p = (a + b + c) / 2;
+
+  let s = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+
+  S.textContent = `Площадь треугольника: ${s}`;
 };
 
-const input1 = document.getElementById("input_d");
-input1.addEventListener("input", calc_S_P);
+const input1 = document.getElementById("input_a");
+input1.addEventListener("input", calc_S);
 
-// Start 17:20
-// Finish 17:38
+const input2 = document.getElementById("input_b");
+input2.addEventListener("input", calc_S);
+
+const input3 = document.getElementById("input_c");
+input3.addEventListener("input", calc_S);
+
+// Start 18:30
+// Finish 18:56
