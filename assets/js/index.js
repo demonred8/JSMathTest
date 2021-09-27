22574726c549abac7f503ea6201a2bf9282cf658
DemonRed <demonred8@gmail.com>
2021-09-27 17:13:36 +0300

    task2

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,15 +1,18 @@
-const calc_S_P = (e) => {
-    const S = document.getElementById('S')
-    const P = document.getElementById('P')
-    let side = e.target.value
+const calc_S_P = () => {
+  const S = document.getElementById("S");
+  const P = document.getElementById("P");
+  let horizontal = input1.value;
+  let vertical = input2.value;
 
-    S.textContent = "Площадь: " + side * side
-    P.textContent = "Периметр: " + side * 4
+  S.textContent = "Площадь: " + horizontal * vertical;
+  P.textContent = "Периметр: " + (horizontal * 2 + vertical * 2);
+};
 
-    console.log(side)
-}
+const input1 = document.getElementById("input_h");
+input1.addEventListener("input", calc_S_P);
 
-const input = document.getElementById('input').addEventListener("input", calc_S_P);
+const input2 = document.getElementById("input_v");
+input2.addEventListener("input", calc_S_P);
 
-
-
+// Start 17:06
+// Finish 17:13
